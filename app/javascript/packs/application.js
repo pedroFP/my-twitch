58e52fedfd88ba69269f2b4978b7c2929079712e
pedroVflight <pedro@vflight.cl>
2021-04-07 02:03:35 -0400

    ➕ add Hls,js and video player

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,19 @@ require("bootstrap")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import Hls from "hls.js/dist/hls"
+
+$(document).on('turbolinks:load', function() {
+  var video = document.getElementById("video");
+  var videoSrc = "http://localhost:8080/hls/test.m3u8";
+  if (Hls.isSupported()) {
+    var hls = new Hls();
+    hls.loadSource(videoSrc);
+    hls.attachMedia(video);
+  }
+
+  else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+    video.src = videoSrc;
+  }
+})
