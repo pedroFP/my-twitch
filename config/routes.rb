f4695b22277a99623062c4478359ed092ebecc79
pedroVflight <pedro@vflight.cl>
2021-04-13 02:36:54 -0400

    ✨ add name attribute to registration form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
