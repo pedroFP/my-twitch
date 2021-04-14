bac89852e8ef82e5275512d8e03c916cdde86694
pedroVflight <pedro@vinkodigital.com>
2021-04-14 00:45:11 -0400

    ✨ add stream auth controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
+  # post 'stream_auth', to: 'stream_auth#create'
+  resource :stream_auth, only: [:create]
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/stream_auth_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StreamAuthControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
