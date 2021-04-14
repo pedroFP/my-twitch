fcca15527ebd0572c92ad404a7e1aaafffb7bf73
pedroVflight <pedro@vinkodigital.com>
2021-04-14 02:15:48 -0400

    🔧  cleared hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Allow action testing (Postman)
+  config.hosts.clear #config.hosts << "localhost"
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
