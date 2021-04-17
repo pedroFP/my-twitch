620687e15a52955864bc1a2ba600511b1e694c91
pedroVflight <pedro@vinkodigital.com>
2021-04-17 04:28:16 -0400

    🐛 fixed host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://pedro-tv.herokuapp.com/', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'pedro-tv.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
     :address => ENV['SMTP_EMAIL'],
