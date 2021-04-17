12178e87e50aa2d9af9e12fd75268eff6f2a4d15
pedroVflight <pedro@vinkodigital.com>
2021-04-17 04:22:30 -0400

    🔧 add heroku host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,6 +109,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'https://pedro-tv.herokuapp.com/', port: 3000 }
+
   config.action_mailer.smtp_settings = {
     :address => ENV['SMTP_EMAIL'],
     :port => 587,
